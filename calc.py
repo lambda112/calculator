2fa1abb662bd02fad19f814b8a0073f3d20ed605
Kamran Azim <Kamazim121212@gmail.com>
2024-02-05 23:49:07 +0000

    Finished fully working version of the project
    Allowed user to continue calculation with current worked out number or start new calc

--- calc.py
+++ calc.py
@@ -1,34 +1,80 @@
+from os import system
+operations = ["+", "-", "*", "/"]
+
+
 def add(n1, n2):
+    """Add two numbers together"""
     return n1 + n2
 
+
 def subtract(n1,n2):
+    """Subtract two numbers together"""
     return n1 - n2
 
+
 def multiply(n1,n2):
+    """Multiply two numbers together"""
     return n1 * n2
 
+
 def divide(n1,n2):
+    """Divide two numbers together"""
+
+    if n1 == 0 or n2 == 0:
+        print(f"Cannot divide by 0!")
+        return 1
+
     return n1 / n2
 
 
-operations = ["+", "-", "*", "/"]
+def calculation(same_value:bool = False, calc:int = 0):
+    
+    while True:
+        try:
+            if same_value:
+                n1 = calc
+                n2 = int(input("Enter second number: "))
+                break
 
-n1 = int(input("Enter first number: "))
-n2 = int(input("Enter second number: "))
-operator = input("Enter an operator\n+\n-\n*\n/\n: ")
+            else:
+                n1 = int(input("Enter first number: "))
+                n2 = int(input("Enter second number: "))
+                break
 
-calc_dict = {
-    "+" : add(n1, n2),
-    "-" : subtract(n1, n2),
-    "*" : multiply(n1,n2),
-    "/" : divide(n1,n2)
-}
+        except ValueError:
+            print("Enter a number please!\n")
 
+
+    operator = input("Enter an operator\n+\n-\n*\n/\n: ")
+    calc_dict = {
+        "+" : add(n1, n2),
+        "-" : subtract(n1, n2),
+        "*" : multiply(n1,n2),
+        "/" : divide(n1,n2)
+    }
+
+    while True:
+        if operator in operations:
+            break
+        else:
+            operator = input("Enter a valid operator\n+\n-\n*\n/\n: ")
+
+    calc = calc_dict[operator]
+    print(f"{calc}\n") 
+    return calc
+
+
+calc = calculation()
 while True:
-    if operator in operations:
-        break
-    else:
-        operator = input("Enter a valid operator\n+\n-\n*\n/\n: ")
 
-calc = calc_dict[operator]
-print(calc)
+    finished = input(f"Type 'y' to continue calculating with {calc} or 'n' to start new calculation: ")
+
+    while finished not in ["y", "n"]:
+        finished = input(f"Type 'y' to continue calculating with {calc} or 'n' to start new calculation: ")
+
+    if finished == "y":
+        calc = calculation(True, calc)
+
+    else:
+        system("cls")
+        calc = calculation()
